44fe8644a5ee4c3b8aa798fca9872afc98e3627f
raeeskhan371 <raeeskhann371@gmail.com>
2025-10-06 17:10:55 +0500

    Complete 50% detail page

--- lib/Widgets/tile2.dart
+++ lib/Widgets/tile2.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:plant_application/pages/colors.dart';
 
-class  tile2 extends StatelessWidget {
-   tile2({super.key,required this.iconePath});
+class tile2 extends StatelessWidget {
+  tile2({super.key, required this.iconePath});
 
-
-
-   String iconePath;
+  String iconePath;
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Container(
       width: 70,
       height: 70,
-      color: Colors.red,
-      child: Image.asset(iconePath)
+      child: Image.asset(iconePath),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Maincolor.withOpacity(0.2),
+            blurRadius: 20,
+            offset: Offset(0, 20),
+          ),
+        ],
+      ),
     );
   }
 }
-
--- lib/pages/DetailViewPage.dart
+++ lib/pages/DetailViewPage.dart
@@ -1,38 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:plant_application/Widgets/tile2.dart';
+import 'package:plant_application/pages/colors.dart';
 
 class Detailviewpage extends StatelessWidget {
   const Detailviewpage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body:Row(
-        children: [
-          // first colum
-          Column(
-            children: [
-              tile2(iconePath: "assets/icons/back_arrow.png"),
-              SizedBox(height: 70,),
-              tile2(iconePath: "assets/icons/sun.png"),
-              SizedBox(height: 70,),
-              tile2(iconePath: "assets/icons/back_arrow.png"),
-            ],
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Row(
+          children: [
+            // first colum
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              child: Column(
+                children: [
+                  Transform.translate( offset: Offset(-20, 0),
+                    child: Container(
+                    width: 70,
+                    height: 70,
+                    child: Image(image: AssetImage("assets/icons/back_arrow.png")),),
+                  ),
+                  SizedBox(height: 80 ),
+                  tile2(iconePath: "assets/icons/sun.png"),
+                  SizedBox(height: 50),
+                  tile2(iconePath: "assets/icons/icon_2.png"),
+                  SizedBox(height: 50),
+                  tile2(iconePath: "assets/icons/icon_3.png"),
+                  SizedBox(height: 50),
+                  tile2(iconePath: "assets/icons/icon_4.png"),
+                ],
+              ),
+            ),
 
-          ),
+            // seconde Colum
+            Column(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      height: 700,
+                      width: 330,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(color: Maincolor.withOpacity(0.3),
+                          blurRadius: 20,
+                          offset: Offset(-5, 10
+                          )),
+                        ],
 
-          // seconde Colum
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(50),
+                          bottomRight: Radius.circular(50),
+                        ),
+                        image: DecorationImage(
 
-          Column(
-            children: [
-
-
-
-            ],
-          )
-        ],
+                          image: AssetImage("assets/images/img.png"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-
     );
   }
 }
